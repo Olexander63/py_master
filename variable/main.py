35402a176f6c1ab786ca3ce88e6e810cef26f52d
Oleksandr Omelchenko <olexander63@gmail.com>
2025-07-06 23:03:24 +0200

    variable

---
+++ variable/main.py
@@ -0,0 +1,42 @@
+# This script prints "Hello, World!" to the console.
+print("lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+# It is a simple demonstration of a Python script.
+
+num_1 = 10
+_num_2 = 20
+
+print(num_1 , _num_2)
+hello = "Hello, World!"
+
+
+# Print the hello variable
+print(hello)
+
+
+x= 100
+print(x)
+
+price = 50.5
+# Print the price variable
+print(price)
+
+active_user = ":TESTING"
+
+# Print the active_user variable
+print(active_user)
+
+DB_USER = "admin"
+# Print the DB_USER variable
+print(DB_USER)
+
+DB_USER=20
+# Print the updated DB_USER variable
+print(DB_USER)
+
+y=10
+y = 20
+# Print the updated y variable
+print(y)
+
+
+
