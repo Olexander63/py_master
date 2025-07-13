bf272c656f686aabf51acba0c0558232e49399d8
Oleksandr Omelchenko <olexander63@gmail.com>
2025-07-13 22:49:25 +0200

    add string1

---
+++ data_type/string/main.py
@@ -0,0 +1,34 @@
+s =''
+s1 = 'hello world1'
+s2 = "hello world2"
+
+#print(s)
+#print(s1)
+#print(s2)
+
+s3= "hello 'world3' "
+print(s3)
+
+print("==========================")
+
+s5='lorem40 ipsum dolor sit amet, consectetur adipiscing elit. \n\
+sed do eiusmod tempor incididunt ut\n\
+labore et dolore magna aliqua.'
+print(s5)
+
+s6 = ('lorem40 ipsum dolor sit amet, consectetur adipiscing elit.')
+print(s6)
+
+print("==========================")
+
+s7 = '''\
+lorem40 ipsum dolor sit amet, consectetur adipiscing elit.\
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'''
+print(s7)
+
+print("==========================")
+
+s8 = 'C:\\Users\\username\\Documents\\file.txt'
+s9 = r'C:\Users\username\Documents\file.txt'
+print(s8)
+print(s9)
