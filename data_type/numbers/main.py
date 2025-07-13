12320e5f090cc2db481e6ffe9e1ca8e3c12ed3ab
Oleksandr Omelchenko <olexander63@gmail.com>
2025-07-13 20:59:39 +0200

    add num

---
+++ data_type/numbers/main.py
@@ -0,0 +1,30 @@
+num1 = 10
+num2 = 2.5
+
+print("Integer:", num1)
+
+print("Float:", num2)
+
+print(20+4) #24
+print(20-4) #16
+print(20*4) #80
+print(20/4) #5.0
+print(20//4) #5
+print(20%4) #0
+print(20%4) #0
+print(20**4) #160000
+print(20+4.5) #24.5
+
+print(-5 -3) # -8
+
+num3 = +3+7*2
+print(num3) # 17
+num4 =(3+7)*2
+print(num4) # 20
+
+print(round(3.14159, 2)) # 3.14
+num5 =1_000_000
+print(num5) # 1000000
+
+num6=1000 ** 1000
+print(type(num6)) # 1000... (very large number)
