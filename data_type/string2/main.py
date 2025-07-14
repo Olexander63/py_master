18c18dab6412ceb1d7d1b0961ac02cc027deace0
Oleksandr Omelchenko <olexander63@gmail.com>
2025-07-14 21:51:05 +0200

    add str2

---
+++ data_type/string2/main.py
@@ -0,0 +1,20 @@
+s1 = 'Hello, ' ' World!'
+print(s1)
+
+print('=' * 20)
+
+s2='Hello'
+s3='World!'
+s4 = s2 + s3 +'||'
+print(s4)
+
+print('=' * 20)
+
+
+product ='Sony'
+price = 1000
+print("Product: " + product + "\n Price: " + str(price))
+
+print('=' * 20)
+
+print(f"Product: {product}\nPrice: {price}")
