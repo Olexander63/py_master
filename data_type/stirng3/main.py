5f11d646a4bd1d7df19d7aff50d881ed81650e54
Oleksandr Omelchenko <olexander63@gmail.com>
2025-07-15 22:26:12 +0200

    add home work

---
+++ data_type/stirng3/main.py
@@ -0,0 +1,23 @@
+s1 ='Hello, world!'
+print(s1[0])  # H
+print(s1[1])  # e
+print(s1[-2])
+print(s1[-1])  # !
+
+
+print('=' * 20 )
+
+
+print(s1)
+print(s1[0:5])  # Hello
+print(s1[7:])  # world!
+print(s1[:5])  # Hello
+print(s1[7:12])  # world
+print(s1[7:12:2])  # wrd
+print(s1[::2])  # Hlo ol!
+print(s1[::-1])  # !dlrow ,olleH
+print(s1[::3])  # Hl r!
+print(s1[::5])  # H ol!
+print(s1[7:12:3])  # wo
+print(s1[7:12:-3])  # (empty string)
+print(s1[7:12:-1])  # (empty string)
