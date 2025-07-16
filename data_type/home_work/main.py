16c40597066994eca3a055a6b28def4d498e9724
Oleksandr Omelchenko <olexander63@gmail.com>
2025-07-16 22:10:47 +0200

    add formating string

---
+++ data_type/home_work/main.py
@@ -0,0 +1,10 @@
+s = 'Hello, World!'
+print(s)
+print(s[12])
+print(s[-1])
+print(s[len(s) - 1 ])
+s_len= len(s)
+print(s_len -1)
+
+print(s[len(s)  -len(s) - 1])
+print(s[-len(s)])
---
+++ format_string/main.py
@@ -0,0 +1,6 @@
+product ='Sony'
+price = 1000
+
+
+print(f'The price of the {product} is {price:.2f} dollars.')
+print('The price of the {} is {} dollars.'.format(product, price))
